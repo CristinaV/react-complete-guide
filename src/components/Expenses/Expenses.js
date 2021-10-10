23edc8518c6cb15b53ade9128d51aebd64c5730e
Cristina Voicu <cristina.voicu@tremend.com>
2021-10-10 14:42:31 +0300

    added chart, worked with for and if in jsx

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import ExpensesFilter from '../ExpensesFilter/ExpenseFilter';
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
   const filterChangeHandler = (year) => {
     setFilteredYear(year);
   };
+
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
   return (
     <Card className="expenses">
       <div>
@@ -17,26 +23,8 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         ></ExpensesFilter>
       </div>
-      <ExpenseItem
-        title={props.items[0].title}
-        date={props.items[0].date}
-        amount={props.items[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        date={props.items[1].date}
-        amount={props.items[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        date={props.items[2].date}
-        amount={props.items[2].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        date={props.items[3].date}
-        amount={props.items[3].amount}
-      ></ExpenseItem>
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
